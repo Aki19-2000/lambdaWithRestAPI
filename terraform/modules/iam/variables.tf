c535c1673fddc4d29938814b4ed70ee5a1825972
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-09 08:54:29 +0530

    Create variables.tf

---
+++ terraform/modules/iam/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "The AWS region"
+  type        = string
+}
+
+variable "account_id" {
+  description = "The AWS account ID"
+  type        = string
+}
+
+variable "lambda_function_name" {
+  description = "The name of the Lambda function"
+  type        = string
+}
